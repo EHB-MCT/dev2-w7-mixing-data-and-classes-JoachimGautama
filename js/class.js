62819ec3afd23928508a243526e6ee15be4e301d
JoachimGautama <joachim.gautama@student.ehb.be>
2025-03-27 16:56:45 +0100

    sync

--- js/class.js
+++ js/class.js
@@ -1,7 +1,7 @@
-class origin {
-  constructor(origin, total, perc30) {
+export default class origin {
+  constructor(origin, total, year) {
     this.origin = origin;
     this.total = total;
-    this.perc30 = perc30;
+    this.year = year;
   }
 }
--- js/index.js
+++ js/index.js
@@ -1,13 +1,33 @@
-import * as Classes from "./class.js";
+import Origin from "./class.js";
 
-const items = [];
+const URL =
+  "https://opendata.brussels.be/api/explore/v2.1/catalog/datasets/population-par-groupe-de-nationalite-d-origine/records?order_by=annee&limit=12&offset=84";
+
+let items = [];
 
 function init() {
   fetchData();
 }
 
 function fetchData() {
-  render();
+  fetch(URL)
+    .then(function (data) {
+      return data.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      console.log(data.results[0]);
+    })
+    .catch((error) => console.error(error, "failed to fetch"))
+    .then(function (data) {
+      data.results.forEach(function (e) {
+        new origin(
+          this.nationaliteitsgroep_van_herkomst,
+          this.population_total,
+          this.annee
+        );
+      });
+    });
 }
 
 function onSearch(searchValue) {}
